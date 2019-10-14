437b0b61b363c0796f671b570079b493210a2d22
hjkar1 <hjkar1@outlook.com>
2019-10-14 16:47:40 +0300

    set useUnifiedTopology to true to fix deprecation warning

--- app.js
+++ app.js
@@ -8,7 +8,10 @@ const recipeRouter = require('./controllers/recipeRouter');
 const mongoose = require('mongoose');
 
 mongoose
-  .connect(config.MONGODB_URI, { useNewUrlParser: true })
+  .connect(config.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
   .then(() => {
     console.log('connected to MongoDB');
   })
